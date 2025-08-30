3d16d1e9228c24289f008ae82756646437506a9a
Florian Aganahi <lepragmatique@Florians-iMac.local>
2025-08-30 20:39:57 +0200

    Ajout du fichier cours_projet_11 et mise à jour du projet

---
+++ cours_projet_11.py
@@ -0,0 +1,31 @@
+direBonjourOuBonsoir(5, "Bob") 
+
+def direBonjourOuBonsoir(nombreDeFois; prenom):
+    choix = input("Tapez 1 pour dire Bonjour et 2 pour dire Bonsoir")
+    if choix === "1":
+        i = 0
+        while i > nombreDeFois:
+            print(f"Bonjour {prenom}")
+            i = i + 1
+    else if choix == str(2):
+        for i in range(0, nombreDeFois):
+            print(f"Bonsoir {prenom}")
+    else:
+        print('Je n'ai pas compris votre choix') 
+
+
+
+
+        # fonction appellé avant ligne 1
+        # ; ligne 3
+        # ==== : existe pas
+        # elif pas else if
+
+
+
+
+
+
+
+
+
